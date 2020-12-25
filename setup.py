a21b3edf29df3a75c16bc6f382b1e379bc1a3f88
Álvaro Fernández Rojas <noltari@gmail.com>
2020-12-25 11:50:03 +0100

    github: support multiple Python versions
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- setup.py
+++ setup.py
@@ -34,6 +34,9 @@ setup(name='AEMET-OpenData',
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9'
       ],
 )
