9049332ab677089b4463e840c39aabbf51f1178c
Álvaro Fernández Rojas <noltari@gmail.com>
2021-01-07 17:05:32 +0100

    interface: remove incorrect function
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- aemet_opendata/interface.py
+++ aemet_opendata/interface.py
@@ -9,7 +9,6 @@ import urllib3
 
 from .const import (
     AEMET_ATTR_DATA,
-    AEMET_ATTR_IDEMA,
     AEMET_ATTR_STATION_LATITUDE,
     AEMET_ATTR_STATION_LONGITUDE,
     AEMET_ATTR_TOWN_LATITUDE_DECIMAL,
@@ -184,17 +183,6 @@ class AEMET:
         if self.debug_api:
             _LOGGER.debug("distance: %s, station: %s", distance, station)
         return station
-
-    # Get conventional observation station data by ID
-    def get_conventional_observation_station_latest_data(self, station_id):
-        """Get latest conventional observation station data by ID"""
-        stations = self.get_conventional_observation_stations()
-        for cur_station in stations[ATTR_DATA]:
-            if cur_station[AEMET_ATTR_IDEMA] == station_id:
-                if self.debug_api:
-                    _LOGGER.debug("station: %s", cur_station)
-                return cur_station
-        return None
 
     # Get conventional observation station data
     def get_conventional_observation_station_data(self, station):
