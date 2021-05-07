af8dddce8b96229ab96274b5900c094ba199e659
Álvaro Fernández Rojas <noltari@gmail.com>
2021-05-07 07:34:57 +0200

    Bump version
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if sys.argv[-1] == 'publish':
 requirements = [x.strip() for x in open(here('requirements.txt')).readlines()]
 
 setup(name='AEMET-OpenData',
-      version='0.2.0',
+      version='0.2.1',
       description='AEMET OpenData Rest API library',
       keywords='aemet',
       author='noltari',
